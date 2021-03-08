0d6c0a254889a0d5ebfeaebe11e4b0b8a046b7a3
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2021-03-08 22:35:19 +0800

    Initial commit (dotnet new webapi)

---
+++ Controllers/SparvPipelineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SparvPipelineProxy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SparvPipelineController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(0);
+        }
+    }
+}
